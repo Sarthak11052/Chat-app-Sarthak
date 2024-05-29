df0f969495e5d753a222f8f61a6b9fe7e6e8f41d
Sarthak <shahsonal353@gmail.com>
2024-05-29 22:39:00 +0530

    first commit

--- api/index.js
+++ api/index.js
@@ -3,19 +3,45 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const Cookies = require('cookies');
+const cookieParser = require('cookie-parser');
 const user = require('./models/Users');
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL);
 const jwtSecret = process.env.JWT_SECRET;
 const app = express();
-app.use(cors());
-app.use(express.json());
+app.use(
+    cors({
+        credentials: true,
+        origin: "http://localhost:5173"
+    })
+);
 
+app.use(express.json());
+app.use(cookieParser());
 
 
 app.get('/test', (req, res) => {
     res.json('test ok');
 });
+
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, JWT_SECRET, {}, (err, userData) => {
+        if (err) throw err;
+        res.json(userData);
+    });
+});
+// app.get('/profile', (req, res) => {
+//     const { token } = req.cookies;
+//     if (!token) {
+//         return res.status(401).json({ error: 'Unauthorized' });
+//     }
+//     jwt.verify(token, jwtSecret, {}, (err, userData) => {
+//         if (err) return res.status(403).json({ error: 'Forbidden' });
+//         res.json(userData);
+//     });
+// });
 
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
@@ -26,7 +52,9 @@ app.post('/register', async (req, res) => {
 
         jwt.sign({ userId: createdUser._id }, jwtSecret, (err, token) => {
             if (err) throw err;
-            res.cookie('token', token).status(201).json('ok');
+            res.cookie('token', token).status(201).json({
+                id: createdUser._id,
+            });
         });
 
 
--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,14 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import axios from "axios";
+import { UserContext } from "./UserContext.jsx";
 function Register() {
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const { setUsername: setLoggedInUsername, setId } = useContext(UserContext);
+
+
     async function register(ev) {
         ev.preventDefault();
-        await axios.post('/register', { username, password });
-
+        const { data } = await axios.post('/register', { username, password });
+        setLoggedInUsername(username);
+        setId(data.id);
     }
+
+
+    // async function register(ev) {
+    //     ev.preventDefault();
+    //     const { data } = await axios.post('/register', { username, password });
+    //     try {
+    //         const { data } = await axios.post('http://localhost:4040/register', { username, password });
+    //         console.log('Registration response:', data);
+    //         setLoggedInUsername(username);
+    //         setId(data.id);
+    //     } catch (error) {
+    //         console.error('Registration error:', error);
+    //     }
+    // }
 
     return (
         <div className="bg-blue-50 h-screen flex items-center">
